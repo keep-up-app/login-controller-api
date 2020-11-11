d5d99ed1283d9eae168c5e24a4952f152e3b17ef
Noah Greff <greffnoah@gmail.com>
2020-11-11 04:42:26 +0100

    first commit

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Controller\ValidationController;
+use App\Controller\ApiController;
+
+class LoginController extends AbstractController
+{
+    private $validator;
+    private $endpoint;
+
+    function __construct()
+    {
+        $this->validator = new ValidationController();
+        $this->endpoint = new ApiController();
+    }
+
+    /**
+     * @Route("/login", name="login_user", methods={"POST"})
+     */
+    public function index(Request $request) : Response
+    {
+        $params = json_decode($request->getContent(), true);
+
+        $resType = 'error';
+        $resContent = 'Internal Server Error';
+        $resCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        if ($this->validator->make($params) == true)
+        {
+            $this->endpoint->request($params);
+
+            if ($this->endpoint->getStatusCode() == 200)
+            {
+                $resType = 'data';
+                $resContent = $this->endpoint->getJsonContent();
+                $resCode = Response::HTTP_OK;
+            } 
+            else 
+            {
+                $resContent = $this->endpoint->getErrorMessage();
+                $resCode = $this->endpoint->getStatusCode();
+            }
+        }
+        else
+        {
+            $resContent = $this->validator->getErrorMessage();
+            $resCode = Response::HTTP_BAD_REQUEST;
+        }
+
+        return new Response(
+            json_encode([ $resType => $resContent ]),
+            $resCode,
+            ['content-type' => 'application/json']
+        );
+    }
+}
---
+++ src/Controller/ValidationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Exception;
+
+class ValidationController extends Exception
+{
+    private $error;
+
+    public function getErrorMessage()
+    {
+        return $this->error;
+    }
+
+    public function make($params)
+    {
+        if ($params == null) return 'Missing fields.';
+
+        foreach($params as $key => $value)
+        {
+            if ($value == null || $value == '')
+            {
+                $this->error = 'Missing ' . ucfirst($key) . '.';
+                return false;
+            }
+        }
+
+        unset($value);
+
+        return true;
+    }
+}
